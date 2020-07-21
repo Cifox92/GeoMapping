c6fb35fef2dbcdfb40d13d4261e7a753071bd374
ignacioserrano9 <serranoi0910@gmail.com>
2020-07-21 12:37:56 +0200

    test map added

--- client/src/components/pages/homeLoggedIn/index.js
+++ client/src/components/pages/homeLoggedIn/index.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 import RouteService from './../../../service/RoutesService'
 import Spinner from 'react-bootstrap/Spinner'
+import MyMapComponent from './map'
 
 class AllRoutes extends Component {
     constructor (props) {
@@ -10,6 +11,10 @@ class AllRoutes extends Component {
         }
         this.routeService = new RouteService()
     }
+
+
+
+
 
     componentDidMount = () => this.updateRouteList()
 
@@ -24,6 +29,20 @@ class AllRoutes extends Component {
         return (
             <>
             <h2>HOLA!</h2>
+             
+             
+            <MyMapComponent
+  isMarkerShown
+  googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
+  loadingElement={<div style={{ height: `100%` }} />}
+  containerElement={<div style={{ height: `400px` }} />}
+  mapElement={<div style={{ height: `100%` }} />}
+/>
+
+
+
+
+
                 {!this.state.routes ? <Spinner animation="grow" role="status"><span className="sr-only">Loading...</span></Spinner> : <ul>{this.state.routes.map(route => <li key={route._id}>{route.name}</li>)}</ul>} 
             </>
         )
