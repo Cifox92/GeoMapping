80856394e30ce49be2ba2d0c7629c102803091a6
ignacioserrano9 <serranoi0910@gmail.com>
2020-07-28 11:12:15 +0200

    fuck ant design

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react'
-import 'antd/dist/antd.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
 import AuthService from './../service/AuthService'
 import { Switch, Route } from 'react-router-dom'
+import Navigation from './ui/Navbar'
 import SignupForm from './auth/Signup-form'
 import LoginForm from './auth/Login-form'
 import ProfilePage from './pages/profile'
@@ -13,32 +14,15 @@ import MyRoutes from './pages/myRoutes'
 import RouteCreation from './pages/routeCreation'
 import RouteDetails from './pages/routeDetails'
 import Edit from './pages/editRoute'
-import { Layout, Menu, Drawer, Button } from 'antd';
 
-const { Header, Content, Footer } = Layout;
-
-
-class App extends React.Component {
+class App extends Component {
   constructor() {
     super()
     this.state = {
       loggedInUser: null,
-      visible: false
     }
     this.AuthService = new AuthService()
   }
-
-  showDrawer = () => {
-    this.setState({
-      visible: true,
-    });
-  };
-
-  onClose = () => {
-    this.setState({
-      visible: false,
-    });
-  };
 
   setTheUser = user => this.setState({ loggedInUser: user }, () => console.log("El estado de App ha cambiado:", this.state))
 
@@ -49,58 +33,28 @@ class App extends React.Component {
       .catch(err => console.log({ err }))
   }
 
-
   render() {
 
     this.fetchUser()
 
     return (
+      <>
+        <Navigation setTheUser={this.setTheUser} loggedInUser={this.state.loggedInUser} />
 
-      <Layout className="layout">
-        <Header>
-          <div className="logo" />          
-          <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
-            <Menu.Item key="1">nav 1</Menu.Item>
-            <Menu.Item key="2">nav 2</Menu.Item>
-            <Menu.Item key="3">nav 3</Menu.Item>
-          </Menu>
-        </Header>
-        <Content style={{ padding: '0 50px' }}>
-          <div className="site-drawer-render-in-current-wrapper">
-            <Switch>
-              <Route exact path='/' render={() => this.state.loggedInUser ? <HomeLoggedIn /> : <HomeNotLoggedIn />} />
-              <Route path='/login' render={props => <LoginForm {...props} setTheUser={this.setTheUser} />} />
-              <Route path='/signup' render={props => <SignupForm {...props} setTheUser={this.setTheUser} />} />
-              <Route path='/profile' render={() => <ProfilePage loggedInUser={this.state.loggedInUser} />} />
-              <Route path='/createRoute' render={props => <CreateRoute {...props} loggedInUser={this.state.loggedInUser} />} />
-              <Route path='/myRoutes' render={props => <MyRoutes {...props} loggedInUser={this.state.loggedInUser} />} />
-              <Route path='/routeCreation/:id' render={props => <RouteCreation {...props} loggedInUser={this.state.loggedInUser} />} />
-              <Route path='/routeDetails/:id' render={props => <RouteDetails {...props} loggedInUser={this.state.loggedInUser} />} />
-              <Route path='/routeEdit/:id' render={props => <Edit {...props} loggedInUser={this.state.loggedInUser} />} />
-            </Switch>
-            <Drawer
-              title="Basic Drawer"
-              placement="left"
-              closable={false}
-              onClose={this.onClose}
-              visible={this.state.visible}
-              getContainer={false}
-              style={{ position: 'absolute' }}
-            >
-              <p>Some contents...</p>
-            </Drawer>
-          </div>
-          <div className="site-layout-content">Content</div>
-        </Content>
-        <Footer style={{ textAlign: 'center' }}>GeoMapping ©2020 Created by Ignacio Serrano & David Roel</Footer>
-      </Layout>
+        <Switch>
+          <Route exact path='/' render={() => this.state.loggedInUser ? <HomeLoggedIn /> : <HomeNotLoggedIn />} />
+          <Route path='/login' render={props => <LoginForm {...props} setTheUser={this.setTheUser} />} />
+          <Route path='/signup' render={props => <SignupForm {...props} setTheUser={this.setTheUser} />} />
+          <Route path='/profile' render={() => <ProfilePage loggedInUser={this.state.loggedInUser} />} />
+          <Route path='/createRoute' render={props => <CreateRoute {...props} loggedInUser={this.state.loggedInUser} />} />
+          <Route path='/myRoutes' render={props => <MyRoutes {...props} loggedInUser={this.state.loggedInUser} />} />
+          <Route path='/routeCreation/:id' render={props => <RouteCreation {...props} loggedInUser={this.state.loggedInUser} />} />
+          <Route path='/routeDetails/:id' render={props => <RouteDetails {...props} loggedInUser={this.state.loggedInUser} />} />
+          <Route path='/routeEdit/:id' render={props => <Edit {...props} loggedInUser={this.state.loggedInUser} />} />
+        </Switch>
+      </>
     )
   }
 }
 
 export default App
-
-
-
-
-
